ed92a9375ef4f4d03050af56bd5ccdc57f5f03c7
yehemin <hemin.ye@qq.com>
2026-04-24 15:47:29 +0800

    feat: add exception classes

---
+++ register/exception.py
@@ -0,0 +1,13 @@
+class RegisterError(Exception):
+    """Base exception for all register errors."""
+    pass
+
+
+class ValidationError(RegisterError):
+    """Raised when type/index validation fails."""
+    pass
+
+
+class DimensionError(RegisterError):
+    """Raised for dimension-related issues."""
+    pass
