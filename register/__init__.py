7b90952504d3dc7503fd278437f46bea87e2fe8f
yehemin <hemin.ye@qq.com>
2026-04-24 17:18:18 +0800

    feat: create public API via __init__.py

--- register/__init__.py
+++ register/__init__.py
@@ -1,1 +1,18 @@
-# Register package
+from .register import Register
+from .parameter import Parameter, Id, Code, Name
+from .dimension import Dimension, Index, Metric
+from .exception import RegisterError, ValidationError, DimensionError
+
+__all__ = [
+    "Register",
+    "Parameter",
+    "Dimension",
+    "Index",
+    "Metric",
+    "Id",
+    "Code",
+    "Name",
+    "RegisterError",
+    "ValidationError",
+    "DimensionError",
+]
