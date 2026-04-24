5761a1feab64e13edb94e69813819924209131c4
yehemin <hemin.ye@qq.com>
2026-04-24 15:55:55 +0800

    feat: add Dimension class with Index and Metric

---
+++ register/dimension.py
@@ -0,0 +1,37 @@
+class Dimension:
+    _name: str
+    _name_cn: str
+    _sign: str
+
+    def __init__(self, name: str, name_cn: str, sign: str):
+        self._name = name
+        self._name_cn = name_cn
+        self._sign = sign
+
+    def __str__(self):
+        return self._sign
+
+    def __repr__(self):
+        return self._name
+
+    def __hash__(self):
+        return hash(self._sign)
+
+    def __eq__(self, other):
+        return isinstance(other, Dimension) and (self._sign == other.sign)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def name_cn(self):
+        return self._name_cn
+
+    @property
+    def sign(self):
+        return self._sign
+
+
+Index = Dimension('Index', '下标', 'IX')
+Metric = Dimension('Metric', '指标汇总', 'MTC')
---
+++ tests/test_dimension.py
@@ -0,0 +1,55 @@
+import pytest
+
+def test_dimension_creation():
+    from register.dimension import Dimension
+    dim = Dimension("test", "测试", "TST")
+    assert dim.name == "test"
+    assert dim.name_cn == "测试"
+    assert dim.sign == "TST"
+
+def test_dimension_str_returns_sign():
+    from register.dimension import Dimension
+    dim = Dimension("test", "测试", "TST")
+    assert str(dim) == "TST"
+
+def test_dimension_repr_returns_name():
+    from register.dimension import Dimension
+    dim = Dimension("test", "测试", "TST")
+    assert repr(dim) == "test"
+
+def test_dimension_hashable():
+    from register.dimension import Dimension
+    dim1 = Dimension("test", "测试", "TST")
+    dim2 = Dimension("test", "测试", "TST")
+    assert hash(dim1) == hash(dim2)
+
+def test_dimension_equality():
+    from register.dimension import Dimension
+    dim1 = Dimension("test", "测试", "TST")
+    dim2 = Dimension("test", "测试", "TST")
+    dim3 = Dimension("other", "其他", "OTH")
+    assert dim1 == dim2
+    assert dim1 != dim3
+
+def test_dimension_equality_based_on_sign():
+    from register.dimension import Dimension
+    dim1 = Dimension("name1", "测试1", "TST")
+    dim2 = Dimension("name2", "测试2", "TST")
+    assert dim1 == dim2  # Equal because signs match
+
+def test_index_dimension_exists():
+    from register.dimension import Index
+    assert Index.name == "Index"
+    assert Index.name_cn == "下标"
+    assert Index.sign == "IX"
+
+def test_metric_dimension_exists():
+    from register.dimension import Metric
+    assert Metric.name == "Metric"
+    assert Metric.name_cn == "指标汇总"
+    assert Metric.sign == "MTC"
+
+def test_dimension_sign_property():
+    from register.dimension import Dimension
+    dim = Dimension("test", "测试", "TST")
+    assert dim.sign == "TST"
