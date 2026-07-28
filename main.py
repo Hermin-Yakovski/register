ec9e562e1343b219acbe2abe82ae80a38dee7081
yehemin <hemin.ye@qq.com>
2026-07-28 13:57:51 +0800

    main

---
+++ main.py
@@ -0,0 +1,11 @@
+from register import Register
+from register.dimension import *
+from register.parameter import *
+
+
+if __name__ == '__main__':
+    reg = Register()
+    reg[Id][Index,][1,] = 1
+    reg[Name][Index,][2,] = 'x'
+
+    exit(0)
