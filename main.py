28802120b8a1283fa02039a11459cb13690afdcb
yehemin <hemin.ye@qq.com>
2026-07-28 14:15:19 +0800

    main ruff format --check

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from register import Register
 from register.dimension import *
 from register.parameter import *
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     reg = Register()
     reg[Id][Index,][1,] = 1
-    reg[Name][Index,][2,] = 'x'
+    reg[Name][Index,][2,] = "x"
 
     sys.exit(0)
